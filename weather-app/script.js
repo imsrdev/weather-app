c40ba3c8f8375525ca9bfac219b56ceebf0253e7
sara <160650459+imsrdev@users.noreply.github.com>
2024-03-02 12:39:03 +0330

    Update script.js

--- weather-app/script.js
+++ weather-app/script.js
@@ -19,15 +19,15 @@ async function checkweather(city) {
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-    if (data.weather[0].main == "Clouds") {
+    if (data.weather[0].main == "clouds") {
       weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main == "clear") {
+    } else if (data.weather[0].main == "Clear") {
       weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main == "rain") {
+    } else if (data.weather[0].main == "Rain") {
       weatherIcon.src = "images/rain.png";
-    } else if (data.weather[0].main == "drizzle") {
+    } else if (data.weather[0].main == "Drizzle") {
       weatherIcon.src = "images/drizzle.png";
-    } else if (data.weather[0].main == "mist") {
+    } else if (data.weather[0].main == "Mist") {
       weatherIcon.src = "images/mist.png";
     }
     document.querySelector(".weather").style.display = "block";
